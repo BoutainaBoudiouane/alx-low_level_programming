5a9849140d977d6447551bad3a43f0e4ee237685
BoutainaBoudiouane <boutainaboudiouane@gmail.com>
2024-08-01 11:35:38 -0700

    task_0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * _strlen - Function to calculate the length of a string.
+ * @s: The input string.
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+	return (length);
+}
+
+/**
+ * print_list - prints a linked lists
+ * @h: pointer to first node
+ * Return: size of list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+		printf("[%u] %s\n", !h->str ? 0 : h->len, h->str ? h->str : "(nil)"),
+			h = h->next, i++;
+	return (i);
+}
